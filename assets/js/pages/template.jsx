8e5f3205dd15f3163aed21db17951a802e02c6d1
Joe Fitter <joefitter@JoesMacBookProII.local>
2018-03-29 16:17:19 +0100

    stash

--- assets/js/pages/template.jsx
+++ assets/js/pages/template.jsx
@@ -1,10 +1,24 @@
 const React = require('react');
 const { render } = require('react-dom');
 const { Provider } = require('react-redux');
+const qs = require('qs');
+const url = require('url');
 const Component = require('../../../views/containers/{{page}}');
 const createStore = require('../../../src/create-store');
 
-const store = createStore(window.INITIAL_STATE);
+const initialFilters = url.parse(window.location.href).query;;
+
+const store = createStore(window.INITIAL_STATE, qs.parse(initialFilters));
+
+store.subscribe(() => {
+  const filters = store.getState().filters;
+  if (!filters.textFilter) {
+    delete filters.textFilter;
+  }
+  const location = url.parse(window.location.href);
+  location.search = qs.stringify(filters);
+  window.history.replaceState(undefined, undefined, location.format());
+});
 
 render(
   <Provider store={store}>
--- src/create-store.js
+++ src/create-store.js
@@ -1,4 +1,14 @@
+const url = require('url');
+const qs = require('qs');
 const { createStore } = require('redux');
 const rootReducer = require('./reducers');
 
-module.exports = data => createStore(rootReducer, { ...data, places: { all: data.places } });
+// if (typeof window === 'undefined') {
+//   console.log(new global.Headers());
+// }
+
+module.exports = (data, initialFilters) => {
+  const stuffs = { ...data, places: { all: data.places }, filters: { ...initialFilters } };
+  console.log(stuffs)
+  return createStore(rootReducer, stuffs);
+}
