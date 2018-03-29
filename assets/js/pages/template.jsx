84cb42450b1ebbce436f2f2651b02d8c16af5824
Joe Fitter <joefitter@JoesMacBookProII.local>
2018-03-29 17:08:47 +0100

    fix lint

--- assets/js/pages/template.jsx
+++ assets/js/pages/template.jsx
@@ -1,3 +1,5 @@
+/* eslint implicit-dependencies/no-implicit: [2, { dev: true }] */
+
 const React = require('react');
 const { render } = require('react-dom');
 const { Provider } = require('react-redux');
--- src/create-store.js
+++ src/create-store.js
@@ -1,6 +1,4 @@
-const url = require('url');
-const qs = require('qs');
-const { merge } = require('lodash')
+const { merge } = require('lodash');
 const { createStore } = require('redux');
 const rootReducer = require('./reducers');
 const filtersReducer = require('./reducers/filters');
@@ -8,4 +6,4 @@ const filtersReducer = require('./reducers/filters');
 module.exports = (data, filterBy) => {
   const filters = merge(filtersReducer(undefined, {}), { filterBy });
   return createStore(rootReducer, { ...data, places: { all: data.places }, filters });
-}
+};
